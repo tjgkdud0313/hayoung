96c852c2d9809235371acf203aa8986eb11b5d33
tjgkdud0313 <tjgkdud0313@naver.com>
2020-04-09 13:40:10 +0900

    commit

---
+++ Homework04/practice01.js
@@ -0,0 +1,45 @@
+//1번 문제
+
+var list = ["h","e","l","l","o","w","o","r","l","d"];
+sum=0;
+for(i=0; i<list.length ;i++){
+    sum++;
+}
+console.log(sum);
+console.log("\n");
+
+
+//2번 문제 
+
+var list2 = [3,5,2,6,8,9,11,7,21,33,56,22,54,20,343,988,1202,81];
+for(i=0;i<list2.length;i++){
+   if(i%2==0){
+       console.log(list2[i]);
+   }
+}
+
+
+//3번 문제
+
+var o_list ={
+    index_01:3,
+    index_02:5,
+    index_03:2,
+    index_04:6,
+    index_05:8,
+    index_06:9,
+    index_07:11,
+    index_08:7,
+    index_09:21,
+    index_10:33,
+    index_11:56,
+    index_12:22,
+    index_13:54,
+    index_14:20,
+    index_15:343,
+    index_16:988,
+    index_17:1202,
+    index_18:81
+}
+
+console.log(o_list);
